eaf512038ef291759f99aef7ed99fb55cc4cd194
Zoe B <zoebscott@hotmail.com>
2022-04-30 21:20:54 +0100

    small styling tweaks added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,24 @@ export const Navbar = () => {
           <img
             id="nutriverse-logo"
             src={process.env.PUBLIC_URL + "/images/nutri-verse-logo.png"}
+            alt="Nutriverse Logo"
           />
           <span id="nutri">nutri</span>
           <span id="verse">verse.</span>
         </h1>
       </div>
       <div id="nav-links">
-        <h2 id="home">home</h2> <h2>|</h2> <h2 id="recipes">recipes</h2>{" "}
-        <h2>|</h2> <h2 id="popular">popular</h2>
+        <div id="home">
+          <h2>Home</h2>
+        </div>
+        <h2 className="pipe"> | </h2>
+        <div id="recipes">
+          <h2>Recipes</h2>
+        </div>
+        <h2 className="pipe"> | </h2>
+        <div id="popular">
+          <h2>Popular</h2>
+        </div>
       </div>
       <div>
         <Button />
@@ -27,7 +37,7 @@ export const Navbar = () => {
 export const Button = () => {
   return (
     <button id="sign-up-button">
-      <h2 id="sign-up">sign-up</h2>
+      <h2 id="sign-up">Sign up</h2>
     </button>
   );
 };
