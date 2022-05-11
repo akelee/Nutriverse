f3782ec47f74e1de1dedc1e199d03103644f2a4d
Alex <alex.spam2510@gmail.com>
2022-05-11 08:48:21 +0200

    Working on styling for the main page

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -9,11 +9,13 @@ const Header = () => {
           <h1 className="main-text1">Meal planning
           </h1>
           <h1 className="main-text2">on the go.</h1>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci, fugit! Aspernatur veniam adipisci soluta esse possimus culpa dolor accusamus ipsum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam facilis, tempora officiis in ea quae suscipit. Tempora aliquid nihil , eos ad eligendi quod ipsam.</p>
+          <p>Taking the first step is never easy - we get it! Planning a full day of nutritional meals may seem impossible, and that's where we come in. Here at nutriverse, we prioritise your health and well-being. We offer highly customised meal planning and nutrient recommendations based on your needs. Say hello to a life-changing platform that lets you to plan your meals on the go.
+          </p>
+          <p>Still unsure? Try it for yourself!</p>
           <button type="button" className="nutriverse__getstarted">Get Started</button>
     </div>
     <div className="nutriverse__header-image">
-      <img src={avo} width="550px" alt="avo" />
+      <img src={avo} width="540px" alt="avo" />
     </div>
     </div>
   )
