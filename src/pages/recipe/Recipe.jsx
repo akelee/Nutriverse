d453da3867ec158a12ba21bd36d0970f0c385bd5
Alex <alex.spam2510@gmail.com>
2022-05-15 23:04:27 +0200

    Adding some Links to Recipe.jsx, which will need to be linked to spoonacular when clicked. I've also added a search bar that could retrieve recipe matches based on the input. What do you think?

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './recipe.css'
+import { Feature } from '../../components'
+import { Link } from 'react-router-dom';
 import nutrient from '../../assets/nutrients.png'
 import ingredients from '../../assets/ingredients.png'
 import scale from '../../assets/scale.png'
@@ -9,18 +11,26 @@ const Recipe = () => {
   return (
     <div className="nutriverse__recipe_container">
       <div className="nutriverse__recipe_info">
-        <div className="nutrients">
-          <img src={nutrient} alt="nutrient" width="280px" /><h2>Search recipes by nutrients</h2>
-        </div>
-        <div className="ingredients">
-        <img src={ingredients} alt="nutrient" width="280px" />
-          <h2>Search by ingredients</h2>
-        </div>
-        <div className="analyserecipe">
-        <img src={scale} alt="nutrient" width="280px" />
-          <h2>Analyse recipe</h2></div>
+        <div className="searchbar">
+          <input placeholder="Search recipes"/>
+          <div><button className="btn">Go</button>
           </div>
-    </div>
+          </div>
+          <div className="recipe-body">
+            <div id="nutrients">
+              <Link to="/"><img src={nutrient} alt="nutrient" width="280px" /><Feature text ="Search recipes by nutrients"/></Link>
+              </div>
+            <div id="ingredients">
+              <Link to="/"><img src={ingredients} alt="nutrient" width="280px" />
+              <Feature text ="Search by ingredients"/></Link>
+              </div>
+            <div id="analyserecipe">
+              <Link to="/"><img src={scale} alt="nutrient" width="280px" />
+              <Feature text ="Analyse recipe"/></Link>
+            </div>
+          </div>
+        </div>
+        </div>
   )
 }
 
