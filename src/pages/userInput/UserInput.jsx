497f034b4c7b44d0956e82496ab6a0e109930b99
Alex <alex.spam2510@gmail.com>
2022-05-16 23:38:22 +0200

    Adding modifications to api

--- src/pages/userInput/UserInput.jsx
+++ src/pages/userInput/UserInput.jsx
@@ -5,11 +5,13 @@ import './userInput.css'
 const UserInput = () => {
   return (
     <div className="nutriverse__userInput-container">UserInput
-        <div>
-
-        </div>
+        {/* { <div>
+        <div id="ss_survey_widget"></div>
+        (function(){var e="ss-widget",t="script",a=document,r=window;var s,n,c;r.SS_WIDGET_TOKEN="tt-ee4b04";r.SS_ACCOUNT="nutriverse.surveysparrow.com";r.SS_SURVEY_NAME="Nutriverse-User-Data";if(!a.getElementById(e)){var S=function(){S.update(arguments)};S.args=[];S.update=function(e){S.args.push(e)};r.SparrowLauncher=S;s=a.getElementsByTagName(t);c=s[s.length-1];n=a.createElement(t);n.type="text/javascript";n.async=!0;n.id=e;n.src=["https://","nutriverse.surveysparrow.com/widget/",r.SS_WIDGET_TOKEN].join("");c.parentNode.insertBefore(n,c)}})();
+        </div> } */}
     </div>
   )
 }
 
 export default UserInput
+
