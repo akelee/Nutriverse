c37b13284139ee5b520418cd0380095bd1539314
Alex <80860409+akelee@users.noreply.github.com>
2022-05-17 23:13:55 +0200

    Update SignUpValidate.jsx

--- src/pages/signUpValidate/SignUpValidate.jsx
+++ src/pages/signUpValidate/SignUpValidate.jsx
@@ -1,5 +1,8 @@
 export default function validateInfo(values) {
     let errors = {}
+    let errors_bool = true
+
+    console.log("Validate info ran");
 
     if(!values.username.trim()) {
         errors.username = "Username required!"
@@ -7,19 +10,21 @@ export default function validateInfo(values) {
 
     if(!values.email()) {
         errors.email = "Email required!"
-    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+    } else if (!/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+i/.test(values.email)) {
         errors.email = 'Email address is invalid'
     }
     if(!values.password) {
         errors.password = "Password is required!"
     } else if (values.password.length < 6) {
-        errors.password = "Password requires 6 or more characters!"
+        document.querySelector(".password-invalid").innerHTML = "Password requires 6 or more characters!"
+        errors_bool = false
     } 
     if(!values.password2) {
-        errors.password2 = "Password is requireed!"
+        errors.password2 = "Password is required!"
     } else if (values.password !== values.password2) {
         errors.password2 = "Passwords do not match!"
     }
+    return errors_bool
 }
 
 // return errors
