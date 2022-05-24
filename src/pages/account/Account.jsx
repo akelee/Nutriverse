e08112390348f2ed6f651ada42c19a0dbb284ac3
Zoe Scott <zobescott@hotmail.com>
2022-05-24 15:51:53 +0100

    updated css in recipeList file, removed redudant import in Account.jsx

--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import UserCard from "../../components/UserCard/UserCard";
-import { Link } from "react-router-dom";
 import "./account.css";
 
 import { getDummyUsers } from "../../api/api";
