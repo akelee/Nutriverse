374f66760cb2df1e089a98ab7eac42498b01d4fc
Zoe Scott <zobescott@hotmail.com>
2022-05-24 13:35:08 +0100

    added username, name & email headers to account page, attempting to hook up to dummy data from api

--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./account.css";
 
+import { getDummyUsers } from "../../api/api";
+const dummyUsers = getDummyUsers();
+
 const AccountPage = () => {
   return (
     <div className="account-container">
       <div className="h2-container">
         <h2>Your account details:</h2>
+        <div className="user-details">
+          <h4>Username: {dummyUsers.username}</h4>
+          <h4>Name: {dummyUsers.display_name}</h4>
+          <h4>Email: {dummyUsers.email}</h4>
+        </div>
       </div>
     </div>
   );
