797db30e33bcbd8f265d4004561681102b7ad00a
Zoe Scott <zobescott@hotmail.com>
2022-05-24 15:12:13 +0100

    hooked up account page to dummy account data & added css styling

--- src/pages/RecipeList.jsx
+++ src/pages/RecipeList.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import './recipeList.css'
+import React from "react";
+import "./recipelist.css";
 
-
-function RecipeList({recipeData}) {
+function RecipeList({ recipeData }) {
   console.log(recipeData.results);
   return (
-    <div className="recipe-results">{recipeData.results.map(result => 
-      <p className="result">{result.title} 
-      <div><img src={result.image} alt="" />
-      </div>
-      <div className="info">
-        <a href={result.sourceUrl} target="_blank">View Recipe Here</a></div></p>)
-          
-        }
-        
-      
-      </div>
-  )
+    <div className="recipe-results">
+      {recipeData.results.map((result) => (
+        <p className="result">
+          {result.title}
+          <div>
+            <img src={result.image} alt="" />
+          </div>
+          <div className="info">
+            <a href={result.sourceUrl} target="_blank">
+              View Recipe Here
+            </a>
+          </div>
+        </p>
+      ))}
+    </div>
+  );
 }
 
 export default RecipeList;
--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import UserCard from "../../components/UserCard/UserCard";
 import { Link } from "react-router-dom";
 import "./account.css";
 
 import { getDummyUsers } from "../../api/api";
-const dummyUsers = getDummyUsers();
 
-const AccountPage = () => {
+function AccountPage() {
+  const dummyUsers = getDummyUsers();
+  const [users, setUsers] = useState(dummyUsers || null);
+
+  // useEffect(() => {
+  //   getDummyUsers().then((users) => {
+  //     console.log(users);
+  //     setUsers(users);
+  //   });
+  // }, []);
+
   return (
     <div className="account-container">
-      <div className="h2-container">
+      <div className="account-details-container">
         <h2>Your account details:</h2>
         <div className="user-details">
-          <h4>Username: {dummyUsers.username}</h4>
-          <h4>Name: {dummyUsers.display_name}</h4>
-          <h4>Email: {dummyUsers.email}</h4>
+          {
+            <UserCard
+              username={users[0].username}
+              display_name={users[0].display_name}
+            />
+          }
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default AccountPage;
